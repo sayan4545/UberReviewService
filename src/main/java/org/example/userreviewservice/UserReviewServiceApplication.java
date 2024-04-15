c2f7c421576c468b9fe321977d1bb49a39284929
sayan4545 <sayanchatterjee3010@gmail.com>
2024-04-16 03:56:43 +0530

    Basic Spring data Jpa configuration done

---
+++ src/main/java/org/example/userreviewservice/UserReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.userreviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserReviewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserReviewServiceApplication.class, args);
+    }
+
+}
