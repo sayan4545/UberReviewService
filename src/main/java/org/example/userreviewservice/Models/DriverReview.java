cc20b8c6a4e9ed8cb8fd267db7a63bb29b9c31fd
sayan4545 <sayanchatterjee3010@gmail.com>
2024-04-20 04:15:18 +0530

    Added @PrimaryKeyJoinColumn(name ="booking_id) annotation to customize the primary key

--- src/main/java/org/example/userreviewservice/Models/DriverReview.java
+++ src/main/java/org/example/userreviewservice/Models/DriverReview.java
@@ -1,12 +1,14 @@
 package org.example.userreviewservice.Models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@PrimaryKeyJoinColumn(name = "driver_review_id")
 public class DriverReview extends Review{
     private String driverReviewComment;
 }
