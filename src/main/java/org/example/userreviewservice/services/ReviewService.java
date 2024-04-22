6c04dd55041bbcc52863cfca9e14caf36f7634e0
sayan4545 <sayanchatterjee3010@gmail.com>
2024-04-22 16:33:41 +0530

    Added dummy data

--- src/main/java/org/example/userreviewservice/services/ReviewService.java
+++ src/main/java/org/example/userreviewservice/services/ReviewService.java
@@ -57,8 +57,8 @@ public class ReviewService implements CommandLineRunner {
 //        }
 //        repositories.deleteById(2l);
         Review r = Review.builder()
-                .Content("Average ride quality")
-                .rating(3.0).build();
+                .Content("Good service")
+                .rating(4.0).build();
 
         repositories.save(r);
 
