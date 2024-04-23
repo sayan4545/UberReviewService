b7d98f555d923870e997ee34689ea6e411676049
sayan4545 <sayanchatterjee3010@gmail.com>
2024-04-24 02:16:07 +0530

    Added @OnetoOne annotation

--- src/main/java/org/example/userreviewservice/Models/Booking.java
+++ src/main/java/org/example/userreviewservice/Models/Booking.java
@@ -1,16 +1,18 @@
 package org.example.userreviewservice.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.*;
 
 import java.util.Date;
 
 @Entity
 public class Booking extends BaseModel{
+    @OneToOne
+    private Review review;
     @Enumerated(value = EnumType.STRING)
     private BookingStatus bookingStatus;
+    @Temporal(value = TemporalType.TIMESTAMP)
     private Date startTime;
+    @Temporal(value = TemporalType.TIMESTAMP)
     private Date endTime;
     private Long totalDistance;
 }
