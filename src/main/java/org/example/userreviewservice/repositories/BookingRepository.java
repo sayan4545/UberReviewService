83063211459a95427ca6d1b8a771d68356865b30
sayan4545 <sayanchatterjee3010@gmail.com>
2024-05-14 03:42:34 +0530

    Added methods in reviewRepository

--- src/main/java/org/example/userreviewservice/repositories/BookingRepository.java
+++ src/main/java/org/example/userreviewservice/repositories/BookingRepository.java
@@ -4,6 +4,7 @@ import org.example.userreviewservice.Models.Booking;
 import org.example.userreviewservice.Models.Driver;
 import org.example.userreviewservice.Models.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +14,5 @@ public interface BookingRepository extends JpaRepository<Booking,Long> {
     List<Booking> findAllByDriverId(Long driverId);
 
     List<Booking> findAllByDriverIn(List<Driver> drivers);
+
 }
--- src/main/java/org/example/userreviewservice/repositories/ReviewRepositories.java
+++ src/main/java/org/example/userreviewservice/repositories/ReviewRepositories.java
@@ -1,13 +1,40 @@
 package org.example.userreviewservice.repositories;
 
 
+import org.example.userreviewservice.Models.Driver;
+import org.example.userreviewservice.Models.Passenger;
 import org.example.userreviewservice.Models.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
 
 @Repository
 
 public interface ReviewRepositories extends JpaRepository<Review,Long> {
 
+    Integer countAllByRatingIsLessThan(Integer givenRating);
+    List<Review> findAllByRatingIsLessThanEqual(Integer givenRating);
+    List<Review> findAllByCreatedAtBefore(Date date);
+    @Query(nativeQuery = true,value = "select * from booking b inner join uber_db_local.booking_review br on b.review_id = br.id where b.id=:bookingId")
+    Review findByBookingId(Long bookingId);
+
+
+
+
+
+
+}
+class Custom{
+    public Driver driver;
+    public Passenger passenger;
+    public Review review;
+    public Custom(Driver driver, Passenger passenger, Review review) {
+        this.driver = driver;
+        this.passenger = passenger;
+        this.review = review;
+    }
 
 }
